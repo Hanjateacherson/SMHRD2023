8d4275317f856059beca3ccbb408839630f5525c
KIM SANG SEOK <74088300+Hanjateacherson@users.noreply.github.com>
2024-03-15 16:27:07 +0900

    Add files via upload

---
+++ Node.js/05.Router/routes/user.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res)=>{
+    res.send(`
+    <html>
+        <body>
+            <h1>User Page</h1>
+        </body>
+    </html>
+    `)
+})
+
+router.get('/:name', (req, res)=>{
+    console.log('라우트 매개변수', req.params);
+    /*
+    (mission) <h1>Welcome name! 💚</h1>
+    */
+    res.send(`
+    <html>
+        <body>
+            <h1>Welcome ${req.params.name}! 💚</h1>
+        </body>
+    </html>
+    `)
+})
+
+module.exports = router;
---
+++ Node.js/06.Template/routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res)=>{
+    console.log('Main Page');
+    // res.render(file)
+    //  => views 내 파일
+    res.render('main', {name : 'heejae park'});
+})
+
+// /mypage => mypage.html
+router.get('/mypage', (req,res)=>{
+    res.render('mypage');
+})
+
+module.exports = router;
